8158dc33efd5a7007a671755477cce869cb83fa8
Farok Hossain <farokafs7@gmail.com>
2024-09-28 20:39:20 +0600

    Hero updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,21 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className=" w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
           Hi, I am Farok <span className="waving-hand">🖐️</span>
         </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+        </Canvas>
       </div>
     </section>
   );
