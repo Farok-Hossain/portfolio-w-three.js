621786c5d921cda6ef71e71e61ce0e21ea092797
Farok Hossain <farokafs7@gmail.com>
2024-09-29 14:14:59 +0600

    Hero updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
 
 const Hero = () => {
   return (
@@ -15,8 +17,15 @@ const Hero = () => {
       </div>
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          <HackerRoom />
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              scale={0.05}
+              position={[0, 0, 0]}
+              rotation={[0, -Math.PI / 2, 0]}
+            />
+            <ambientLight intensity={1} />
+          </Suspense>
         </Canvas>
       </div>
     </section>
